680f590a566362a0a34d5187ecf825c4f0e72747
TamarPazTsuk <tamar.paz123@gmail.com>
2022-08-06 19:32:05 +0300

    initial facial feature mapping

--- facial_expressions.py
+++ facial_expressions.py
@@ -1,35 +1,64 @@
-#Importing the necessary packages and libaries
+#Importing the necessary packages and libraries
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn import svm, datasets
 import matplotlib.pyplot as plt
 import numpy as np
+import workshop_utils
 
-# eyebrows & eyes
-## left eyebrow:
-#### center 19
-#### left edge 17
-#### right edge 21
 
-## right eyebrow:
-#### center 24
-#### left edge 22
-#### right edge 26
+# distance between the center of the eyebrow and the eye
+def left_eyebrow_center_to_eye_dist():
+    return
 
-## left eye
-#### upper eyelib 37, 38
-#### lower eyelib 40, 41
-#### center 68
-#### left edge 36
-#### right edge 39
+# distance between the center and the edges of each eyebrow
 
-## right eye
-#### upper eyelib 43, 44
-#### lower eyelib 46, 47
-#### center 69
-#### left edge 42
-#### right edge 45
+# distance between the eyebrows
 
-## Distance between the center of the eyebrow and the eye
+# distance between the edges line and the upper lip (bottom)
 
-## distance between the center and the edges of each eyebrow
+# distance between the edges line and the lower lip (top)
+
+if __name__ == '__main__':
+    # get relevant dict for all frames
+    names = ['left_eyebrow_center',
+             'left_eyebrow_left_edge',
+             'left_eyebrow_right_edge',
+             'right_eyebrow_center',
+             'right_eyebrow_left_edge',
+             'right_eyebrow_right_edge',
+             'left_eye_upper_eyelid_1',
+             'left_eye_upper_eyelid_2',
+             'left_eye_lower_eyelid_1',
+             'left_eye_lower_eyelid_2',
+             'left_eye_center',
+             'left_eye_left_edge',
+             'left_eye_right_edge',
+             'right_eye_upper_eyelid_1',
+             'right_eye_upper_eyelid_2',
+             'right_eye_lower_eyelid_1',
+             'right_eye_lower_eyelid_2',
+             'right_eye_center',
+             'right_eye_left_edge',
+             'right_eye_right_edge',
+             'mouth_upper_lip_bottom_1',
+             'mouth_upper_lip_bottom_2',
+             'mouth_upper_lip_bottom_3',
+             'mouth_upper_lip_top_1',
+             'mouth_upper_lip_top_2',
+             'mouth_upper_lip_top_3',
+             'mouth_lower_lip_bottom_1',
+             'mouth_lower_lip_bottom_2',
+             'mouth_lower_lip_bottom_3',
+             'mouth_lower_lip_top_1',
+             'mouth_lower_lip_top_2',
+             'mouth_lower_lip_top_3',
+             'mouth_left_edge',
+             'mouth_right_edge']
+    numbers = [19, 17, 21, 24, 22, 26, 37, 38, 40, 41, 68, 36, 39, 43, 44, 46,
+               47, 69, 42, 45, 61, 62, 63, 50, 51, 52, 56, 57, 58, 48, 54]
+
+    infant_dict = workshop_utils.extract_coordinates_for_all_frames(0, 2, 'face_keypoints_2d' ,names, numbers)
+    adult_dict = workshop_utils.extract_coordinates_for_all_frames(1, 2, 'face_keypoints_2d', names, numbers)
+
+    features_matrix = []
