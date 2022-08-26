ed73f2445c896a10cc5522d66b66e652a3297c7b
TamarPazTsuk <tamar.paz123@gmail.com>
2022-08-26 13:12:35 +0300

    26/8 path fix

--- facial_expressions.py
+++ facial_expressions.py
@@ -76,7 +76,7 @@ def extract_features_from_coordinates(infant_dict, frames_num):
     #adult_features_matrix.append(mouth_edges_to_lower_lip_angle(adult_dict, frames_num))
     return infant_features_matrix
 
-def get_feature_matrices():
+def get_feature_matrices(sub_no, frames_num):
     # get relevant dict for all frames
     names = ['left_eyebrow_center',
              'left_eyebrow_left_edge',
@@ -114,12 +114,12 @@ def get_feature_matrices():
              'mouth_right_edge']
     numbers = [19, 17, 21, 24, 22, 26, 37, 38, 40, 41, 68, 36, 39,
                43, 44, 46, 47, 69, 42, 45, 61, 62, 63, 50, 51, 52, 65, 66, 67, 56, 57, 58, 48, 54]
-
-    infant_dict = extract_coordinates_for_all_frames(0, 2, 'face_keypoints_2d' ,names, numbers)
+    frames_num = frames_num
+    infant_dict = extract_coordinates_for_all_frames(person_id=0, start_from_frame=0, until_frame=frames_num, body_part='face_keypoints_2d' ,names=names, points=numbers, subject=sub_no)
     #adult_dict = extract_coordinates_for_all_frames(1, 2, 'face_keypoints_2d', names, numbers)
     infant_features_matrix = []
     adult_features_matrix = []
-    frames_num = len((infant_dict['left_eyebrow_center']))
+
     #infant_features_matrix, adult_features_matrix = extract_features_from_coordinates(infant_dict, adult_dict, frames_num)
     infant_features_matrix, adult_features_matrix = extract_features_from_coordinates(infant_dict, frames_num)
     infant_features_matrix = np.concatenate(infant_features_matrix, axis=1)
@@ -129,7 +129,7 @@ def get_feature_matrices():
 
 
 if __name__ == '__main__':
-    infant_x = get_feature_matrices()
+    infant_x = get_feature_matrices(frames_num=3000, sub_no='611_3m')
     y = get_labels_from_file(file_path='ep 1.xlsx')
     X_train, X_test, y_train, y_test = split_data(infant_x, y, train_ratio=0.2)
     classifier, accuracy = run_svm_classifier(X_train, X_test, y_train, y_test, kernel='linear')
